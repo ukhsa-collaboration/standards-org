9078fe3300bc8af83dd3b84fd7d00b95481072d5
Jake Aitchison <jake.aitchison@answerdigital.com>
2025-08-05 10:26:42 +0100

    test: add tests for markdown-it headings rules and utils

---
+++ test/markdown-it/heading.js
@@ -0,0 +1,64 @@
+import assert from 'node:assert/strict'
+import { describe, it, before } from 'node:test'
+import markdownit from 'markdown-it';
+
+import { headingRules } from '../../src/markdown-it/heading.js';
+
+// avoid new instance of MarkdownIt for each call
+describe('markdown-it', () => {
+
+  let md;
+
+  before(() => {
+    // Set up the markdown-it instance with the heading rules
+    md = new markdownit().use(headingRules);
+  });
+
+  it('Does not render headings if options.useMarkdownHeaderAsTitle is true', () => {
+    // arrange
+    const options = { useMarkdownHeaderAsTitle: true } // global options
+    const frontMatter = { options } // page front matter
+
+    // act
+    const result = md.render('# Heading', frontMatter)
+
+    // assert
+    assert.equal(result, '')
+  })
+
+  it('Renders headings if options.useMarkdownHeaderAsTitle is false', () => {
+    // arrange
+    const options = { useMarkdownHeaderAsTitle: false } // global options
+    const frontMatter = { options } // page front matter
+
+    // act
+    const result = md.render('# Heading', frontMatter)
+
+    // assert
+    assert.match(result, /<h1\>Heading/)
+  })
+
+  it('Renders headings if front matter useMarkdownHeaderAsTitle is false', () => {
+    // arrange
+    const options = { useMarkdownHeaderAsTitle: true } // global options
+    const frontMatter = { options, useMarkdownHeaderAsTitle: false } // page front matter
+
+    // act
+    const result = md.render('# Heading', frontMatter)
+
+    // assert
+    assert.match(result, /<h1\>Heading/)
+  })
+
+  it('Does not render headings if front matter useMarkdownHeaderAsTitle is true', () => {
+    // arrange
+    const options = { useMarkdownHeaderAsTitle: false } // global options
+    const frontMatter = { options, useMarkdownHeaderAsTitle: true } // page front matter
+
+    //act 
+    const result = md.render('# Heading', frontMatter)
+
+    // assert
+    assert.equal(result, '')
+  })
+})
---
+++ test/utils.js
@@ -0,0 +1,83 @@
+import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
+
+import { getDefaultTitle } from '../src/utils.js'
+
+describe('getDefaultTitle utility', () => {
+  it('Returns the extracted title from markdown', () => {
+    // arrange
+    const data = { page: { rawInput: '# Test Title' }, options: { useMarkdownHeaderAsTitle: true } }
+
+    //act
+    const result = getDefaultTitle(data)
+
+    // assert
+    assert.equal(result, 'Test Title')
+  })
+
+  it('Returns the extracted title from markdown regardless of additional text formatting', () => {
+    // arrange
+    const data = { page: { rawInput: '# Test Title **bold** `stuff`' }, options: { useMarkdownHeaderAsTitle: true } }
+
+    // act
+    const result = getDefaultTitle(data)
+
+    // assert
+    assert.equal(result, 'Test Title bold stuff')
+  })
+
+  it('Returns explicit title if provided and options.useMarkdownHeaderAsTitle is true', () => {
+    // arrange
+    const data = { title: 'Explicit Title', page: { rawInput: '# Test Title' }, options: { useMarkdownHeaderAsTitle: true } }
+
+    // act
+    const result = getDefaultTitle(data)
+
+    // assert
+    assert.equal(result, 'Explicit Title')
+  })
+
+  it('Returns explicit title if provided and options.useMarkdownHeaderAsTitle is false', () => {
+    // arrange
+    const data = { title: 'Explicit Title', page: { rawInput: '# Test Title' }, options: { useMarkdownHeaderAsTitle: false } }
+
+    // act
+    const result = getDefaultTitle(data)
+
+    // assert
+    assert.equal(result, 'Explicit Title')
+  })
+
+  it('Returns null if no title can be extracted', () => {
+    // arrange
+    const data = { page: { rawInput: '' }, options: { useMarkdownHeaderAsTitle: true } }
+
+    // act
+    const result = getDefaultTitle(data)
+
+    // assert
+    assert.equal(result, null)
+  })
+
+  it('Returns undefined if eleventyExcludeFromCollections is true', () => {
+    // arrange
+    const data = { eleventyExcludeFromCollections: true, page: { rawInput: '# Test Title' }, options: { useMarkdownHeaderAsTitle: true } }
+
+    // act
+    const result = getDefaultTitle(data)
+
+    // assert
+    assert.equal(result, undefined)
+  })
+
+  it('Returns undefined if options.useMarkdownHeaderAsTitle is false and no title is explicitly set', () => {
+    // arrange
+    const data = { page: { rawInput: '# Test Title' }, options: { useMarkdownHeaderAsTitle: false } }
+
+    // act
+    const result = getDefaultTitle(data)
+
+    // assert
+    assert.equal(result, undefined)
+  })
+})
