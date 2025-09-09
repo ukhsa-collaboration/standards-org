84e95298b950ab90655b25f88597a40b59e4327a
Josh Humphries <josh.humphries@ukhsa.gov.uk>
2025-09-09 11:23:05 +0100

    build: update standards-development fetch branch to main

--- src/build/obtain-docs.js
+++ src/build/obtain-docs.js
@@ -6,7 +6,7 @@ import generateFileMetadata from "./generate-docs-file-meta.js";
 
 const repos = [
   { url: 'https://github.com/ukhsa-collaboration/standards-api.git', branch: 'main', outputDirName: 'api-design-guidelines' },
-  { url: 'https://github.com/ukhsa-collaboration/standards-development.git', branch: 'feature/eleventy', outputDirName: 'development-standards' },
+  { url: 'https://github.com/ukhsa-collaboration/standards-development.git', branch: 'main', outputDirName: 'development-standards' },
   // Add more repos as needed
 ];
 
